41385f1f214c4e542b949ed8930ebc5009d016e0
Golobon <golobon@ya.ru>
2024-01-01 20:31:06 +0300

    ListView

---
+++ less_55/src/main/java/com/example/less_55/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.less_55;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    final String LOG_TAG = "meLogs";
+    String[] data = {"one", "two", "three", "four", "five",};
+    ListView lvMain;
+    ArrayAdapter<String> adapter;
+
+    View header1;
+    View header2;
+    View footer1;
+    View footer2;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.but1);
+        b.setOnClickListener(v -> {
+            lvMain.removeHeaderView(header2);
+            lvMain.removeFooterView(footer2);
+        });
+
+        lvMain = findViewById(R.id.lv_main);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, data);
+
+        header1 = createHeader("header 1");
+        header2 = createHeader("header 2");
+        footer1 = createFooter("footer 1");
+        footer2 = createFooter("footer 2");
+
+        fillList();
+    }
+    void fillList(){
+        try{
+            lvMain.addHeaderView(header1);
+            lvMain.addHeaderView(header2, "some text for header 2", false);
+            lvMain.addFooterView(footer1);
+            lvMain.addFooterView(footer2, "some text for footer 2", false);
+            lvMain.setAdapter(adapter);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
+    View createHeader(String text) {
+        View v = getLayoutInflater().inflate(R.layout.header, null);
+        ((TextView) v.findViewById(R.id.tvh_text)).setText(text);
+        return v;
+    }
+    View createFooter(String text) {
+        View v = getLayoutInflater().inflate(R.layout.footer, null);
+        ((TextView) v.findViewById(R.id.tvf_text)).setText(text);
+        return v;
+    }
+}
