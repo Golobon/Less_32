5b48371273565cc48a8b3e0cff4e2e70ead89fa5
Golobon <golobon@ya.ru>
2024-01-01 20:40:59 +0300

    ListView

--- less_55/src/main/java/com/example/less_55/MainActivity.java
+++ less_55/src/main/java/com/example/less_55/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.HeaderViewListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -31,8 +32,18 @@ public class MainActivity extends AppCompatActivity {
 
         b = findViewById(R.id.but1);
         b.setOnClickListener(v -> {
-            lvMain.removeHeaderView(header2);
-            lvMain.removeFooterView(footer2);
+            Object obj;
+            HeaderViewListAdapter hvlAdapter = (HeaderViewListAdapter) lvMain.getAdapter();
+            obj = hvlAdapter.getItem(1);
+            Log.d(LOG_TAG, "hvlAdapter.getItem(1) = " + obj.toString());
+            obj = hvlAdapter.getItem(4);
+            Log.d(LOG_TAG, "hvlAdapter.getItem(4) = " + obj.toString());
+
+            ArrayAdapter<String> alAdapter = (ArrayAdapter<String>) hvlAdapter.getWrappedAdapter();
+            obj = alAdapter.getItem(1);
+            Log.d(LOG_TAG, "alAdapter.getItem(1) = " + obj.toString());
+            obj = alAdapter.getItem(4);
+            Log.d(LOG_TAG, "alAdapter.getItem(4) = " + obj.toString());
         });
 
         lvMain = findViewById(R.id.lv_main);
