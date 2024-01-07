08c1ad10918e582233f0d80af2978f4d2e433b4b
Golobon <golobon@ya.ru>
2024-01-07 22:55:04 +0300

    ListView

--- less_70_Activity_onsaveinstancestate/src/main/java/com/example/less_70_activity_onsaveinstancestate/MainActivity.java
+++ less_70_Activity_onsaveinstancestate/src/main/java/com/example/less_70_activity_onsaveinstancestate/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
