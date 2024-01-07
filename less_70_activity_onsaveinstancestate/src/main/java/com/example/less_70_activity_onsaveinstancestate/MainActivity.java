1c0558c95731a7d78a4e5a5313a15fb258b878c8
Golobon <golobon@ya.ru>
2024-01-07 22:49:22 +0300

    ListView

---
+++ less_70_activity_onsaveinstancestate/src/main/java/com/example/less_70_activity_onsaveinstancestate/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.less_70_activity_onsaveinstancestate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    final String LOG_TAG = "myLogs";
+    int cnt = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.but_count).setOnClickListener(v -> MainActivity.this.onClick());
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        cnt = savedInstanceState.getInt("count");
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", cnt);
+        Log.d(LOG_TAG, "onSaveInstanceState");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+   protected void onClick() {
+       Toast.makeText(this, "Count = " + ++cnt, Toast.LENGTH_LONG).show();
+    }
+
+}
