d5a38b9085bada2c39f46b3c712b6c0382b24d55
Golobon <golobon@ya.ru>
2024-01-09 20:26:39 +0300

    ListView

--- less_72_preferences_list/src/main/java/com/example/less_72_preferences_list/MainActivity.java
+++ less_72_preferences_list/src/main/java/com/example/less_72_preferences_list/MainActivity.java
@@ -6,11 +6,8 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
