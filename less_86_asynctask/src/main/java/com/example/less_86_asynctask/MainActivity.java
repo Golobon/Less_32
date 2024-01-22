5e667bdc7ec38dba81f26bb258220903c5fd84c4
Golobon <golobon@ya.ru>
2024-01-22 20:46:09 +0300

    ListView

--- less_86_asynctask/src/main/java/com/example/less_86_asynctask/MainActivity.java
+++ less_86_asynctask/src/main/java/com/example/less_86_asynctask/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.less_86_asynctask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    MyTask mt;
+    TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = findViewById(R.id.tv_info);
+        findViewById(R.id.btn_start).setOnClickListener(v -> {
+            mt = new MyTask();
+            mt.execute();
+        });
+    }
+
+    public class MyTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvInfo.setText("Begin");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (Exception ignore) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            tvInfo.setText("End");
+        }
     }
 }
