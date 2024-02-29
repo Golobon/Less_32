c8c9d28fa469970b80fbd00f5549c573bc5f8354
Golobon <golobon@ya.ru>
2024-02-29 23:09:48 +0300

    ListView

--- less_132_camerascreen/build.gradle.kts
+++ less_132_camerascreen/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.less_132_camerascreen"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.less_132_camerascreen"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
